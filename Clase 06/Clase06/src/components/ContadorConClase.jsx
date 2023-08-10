95b39d3ad2ee86209245e293c96ccae57b401898
Leonel Martin <leo.mezanzano@live.com.ar>
2023-08-10 19:19:48 -0300

    clase 06

---
+++ Clase 06/Clase06/src/components/ContadorConClase.jsx
@@ -0,0 +1,16 @@
+import { Component } from "react"; 
+
+class ContadorConClase extends Component {
+    constructor() {
+        
+        super();
+
+        this.state = {
+            count: 0,
+        }
+    }
+
+    render() {
+        return 
+    }
+}
