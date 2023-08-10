7f1f6c3295322e968ac9e4d96c0058a115657b49
Leonel Martin <leo.mezanzano@live.com.ar>
2023-08-10 15:19:00 -0300

    clase 03 estilando

--- Clase 03/estilando/src/App.jsx
+++ Clase 03/estilando/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const products = [
+    { id: 1, name: "Iphone", price: 800, stock: 10 },
+    { id: 2, name: "Macbook Pro", price: 1200, stock: 5 },
+    { id: 3, name: "Ipad", price: 600, stock: 2 },
+    { id: 4, name: "Apple Watch", price: 400, stock: 0 },
+    { id: 5, name: "Macbook Air", price: 1000, stock: 8 },
+    { id: 6, name: "Airpods", price: 200, stock: 10 },
+    { id: 7, name: "Magic Mouse", price: 100, stock: 0 },
+    { id: 8, name: "Magic Keyboard", price: 150, stock: 10 },
+  ];
+
+  const productListStyle = {
+    display: "grid",
+    gridTemplateColumns: "repeat(4, 1fr)",
+    gap: 16,
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      style={{
+        width: "100%",
+        maxWidth: "1280px",
+        margin: "0 auto",
+        padding: 16,
+      }}
+    >
+      <h1>Products</h1>
+      <div style={productListStyle}>
+        {products.map((product) => (
+          <div key={product.id} className={styles.productItem}>
+            <img
+              src={`https://picsum.photos/id/${product.id}/300/300`}
+              alt={product.name}
+              style={{ width: "100%" }}
+            />
+            <span className={styles.productTitle}>{product.name}</span>
+            <span>{product.price}</span> <span>{product.stock}</span>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
